46543212c311f21b824382bdd8755062402211f4
azragamel <m_a_justice@yahoo.com>
2020-05-29 17:00:40 -0700

    fixed env and newrelic finally

--- newrelic.js
+++ newrelic.js
@@ -1,6 +1,6 @@
 'use strict'
-require('dotenv');
-const NEW_RELIC = process.env;
+require('dotenv').config();
+
 /**
  * New Relic agent configuration.
  *
@@ -15,7 +15,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: `${NEW_RELIC}`,
+  license_key: process.env.NEW_RELIC,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
